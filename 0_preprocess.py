d027b8192b5ef8591f8d102d9d657cc5b586a823
Leander Lacroix <leander.lacroix.dev@protonmail.com>
2021-02-16 15:30:33 +0000

    Masking of the magnitude map is now done in the dti image space.

--- 0_preprocess.py
+++ 0_preprocess.py
@@ -44,32 +44,10 @@ def preprocess(subject):
     # Generate mask on the 4 b0 images
     b0_file = subject['path'].joinpath(subject['t'] + "_b0.nii.gz")
     b0_mean_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_b0_mean.nii.gz")
-    #b0_mask_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_b0_mask.nii.gz")
 
     subprocess.run(["fslroi", subject['dti'], b0_file, "0", "4"])
     subprocess.run(["fslmaths", b0_file, "-Tmean", b0_mean_file])
-    # subprocess.run(["bet", b0_mean_file, subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_b0"), "-m", "-S", "-f", "0.25"])
-    # subprocess.run(["fslmaths", b0_mask_file, "-fillh", b0_mask_file])
-    # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-dilM", b0_mask_file])
-    # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-dilM", b0_mask_file])
-    # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-dilM", b0_mask_file])
-    # #subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-fillh", b0_mask_file])
-    # #subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-fillh", b0_mask_file])
-    # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "sphere", "10", "-ero", b0_mask_file])
-
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-fillh", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-dilM", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-dilM", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-dilM", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-kernel", "2D", "-ero", b0_mask_file])
-    # # subprocess.run(["fslmaths", b0_mask_file, "-ero", b0_mask_file])
     b0_file.unlink()
-    #b0_mean_file.unlink()
 
     # Compute masked magnitude
     # First split the magnitude file
@@ -82,42 +60,28 @@ def preprocess(subject):
     mag_file = subject['path'].joinpath(subject['t'] + "_mag.nii.gz")
     phase_file = subject['path'].joinpath(subject['t'] + "_phase.nii.gz")
 
-    subprocess.run(["flirt", "-in", b0_mag_0_file, "-ref", b0_mean_file, "-out", mag_file, "-interp", "nearestneighbour", "-applyxfm", "-usesqform", "-datatype", "short"])
-    subprocess.run(["flirt", "-in", subject['phase'], "-ref", b0_mean_file, "-out", phase_file, "-interp", "nearestneighbour", "-applyxfm", "-usesqform", "-datatype", "short"])
+    subprocess.run(["flirt", "-in", b0_mag_1_file, "-ref", b0_mean_file, "-out", mag_file, "-interp", "trilinear", "-applyxfm", "-usesqform", "-datatype", "short"])
+    subprocess.run(["flirt", "-in", subject['phase'], "-ref", b0_mean_file, "-out", phase_file, "-interp", "trilinear", "-applyxfm", "-usesqform", "-datatype", "short"])
 
     mask_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_mask.nii.gz")
-    subprocess.run(["bet2", mag_file, mag_file, "-m", "-f", "0.6", "-g", "-0.1"])
-    mask_file = subject['path'].joinpath(subject['t'] + "_mag_mask.nii.gz")
-    mask_file.rename(subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_mask.nii.gz"))
-    exit()
+    subprocess.run(["bet2", mag_file, subject['path'].joinpath(subject['id'] + "_" + subject['t']), "-m", "-f", "0.65", "-g", "-0.1", "-n", "-w", "2"])
+    mask_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_mask.nii.gz")
+    subprocess.run(["fslmaths", mask_file, "-kernel", "sphere", "5", "-ero", mask_file])
+    subprocess.run(["fslmaths", mask_file, "-kernel", "sphere", "7", "-dilF", mask_file])
+    #subprocess.run(["fslmaths", mask_file, "-kernel", "sphere", "", "-ero", mask_file])
 
     # Mask mag
-    subprocess.run(["fslmaths", mag_file, "-mas", b0_mask_file, mag_file])
+    subprocess.run(["fslmaths", mag_file, "-mas", mask_file, mag_file])
 
+    # Compute fieldmap
     fieldmap_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_fieldmap.nii.gz")
     subprocess.run(["fsl_prepare_fieldmap", "SIEMENS", phase_file, mag_file, fieldmap_file, "2.46"])
 
     # Convert rad.s-1 into Hz
     subprocess.run(["fslmaths", fieldmap_file, "-mul", "0.159155", fieldmap_file])
 
-    # # Ressample b0 mask into mag file resolution
-    # b0_ressampled_mask_file = subject['path'].joinpath(b0_mag_name + "_b0_ressampled_mask.nii.gz")
-    # subprocess.run(["flirt", "-in", b0_mask_file, "-ref", b0_mag_0_file, "-out", b0_ressampled_mask_file, "-applyxfm", "-usesqform"])
-
-    # # Mask mag
-    # subprocess.run(["fslmaths", b0_mag_0_file, "-mas", b0_ressampled_mask_file, b0_mag_0_file])
-
-    # fieldmap_file = subject['path'].joinpath(subject['id'] + "_" + subject['t'] + "_fieldmap.nii.gz")
-    # subprocess.run(["fsl_prepare_fieldmap", "SIEMENS", subject['phase'], b0_mag_0_file, fieldmap_file, "2.46"])
-
-    # # Upsample fieldmap
-    # subprocess.run(["flirt", "-in", fieldmap_file, "-ref", b0_mask_file, "-out", fieldmap_file, "-applyxfm", "-usesqform"])
-
-    # # Convert rad.s-1 into Hz
-    # subprocess.run(["fslmaths", fieldmap_file, "-mul", "0.159155", fieldmap_file])
-
-    # b0_mag_0_file.unlink()
-    # b0_mag_1_file.unlink()
+    b0_mag_0_file.unlink()
+    b0_mag_1_file.unlink()
     # b0_ressampled_mask_file.unlink()
 
 
